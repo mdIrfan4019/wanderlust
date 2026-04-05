4696696e49e5a56e869c76f42d5d61ee33edf4d1
Md Irfan <mia2823457@gmail.com>
2026-04-06 03:05:32 +0530

    dates overlapping handled

--- routes/booking.js
+++ routes/booking.js
@@ -21,30 +21,74 @@ router.post("/", isLoggedIn,isUser, async (req, res) => {
 
   const listing = await Listing.findById(listingId);
 
-  // 🧠 Calculate days
-  const days = Math.ceil(
-    (new Date(checkOut) - new Date(checkIn)) / (1000 * 60 * 60 * 24)
-  );
+  const start = new Date(checkIn);
+  const end = new Date(checkOut);
 
-  if (days <= 0) {
-    req.flash("error", "Invalid dates selected!");
+  // ❌ Invalid date check
+  if (end <= start) {
+    req.flash("error", "Invalid date selection!");
     return res.redirect(`/listings/${listingId}`);
   }
+
+  // 🔥 CHECK OVERLAP
+  const overlappingBooking = await Booking.findOne({
+    listing: listingId,
+    $or: [
+      {
+        checkIn: { $lt: end },
+        checkOut: { $gt: start }
+      }
+    ]
+  });
+
+  if (overlappingBooking) {
+    req.flash("error", "Selected dates are already booked!");
+    return res.redirect(`/listings/${listingId}`);
+  }
+
+  // ✅ Calculate days
+  const days = Math.ceil(
+    (end - start) / (1000 * 60 * 60 * 24)
+  );
 
   const totalPrice = days * listing.price;
 
   const newBooking = new Booking({
     listing: listingId,
     user: req.user._id,
-    checkIn,
-    checkOut,
+    checkIn: start,
+    checkOut: end,
     totalPrice,
   });
 
   await newBooking.save();
 
-  req.flash("success", `Booking confirmed for ${days} days!`);
+  req.flash("success", "Booking confirmed!");
   res.redirect(`/listings/${listingId}`);
+});
+
+router.get("/my", isLoggedIn, isUser, async (req, res) => {
+  const bookings = await Booking.find({ user: req.user._id })
+    .populate("listing");
+
+  res.render("bookings/index.ejs", { bookings });
+});
+
+router.delete("/:id", isLoggedIn, isUser, async (req, res) => {
+  const { id } = req.params;
+
+  const booking = await Booking.findById(id);
+
+  //  check
+  if (!booking || !booking.user.equals(req.user._id)) {
+    req.flash("error", "Unauthorized action!");
+    return res.redirect("/bookings/my");
+  }
+
+  await Booking.findByIdAndDelete(id);
+
+  req.flash("success", "Booking cancelled!");
+  res.redirect("/bookings/my");
 });
 
 export default router;
